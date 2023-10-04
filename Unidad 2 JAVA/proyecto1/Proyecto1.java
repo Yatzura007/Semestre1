db9abb85b3b069243c35a71a787783dbe90feb8b
UrielSerrato <146786194+UrielSerrato@users.noreply.github.com>
2023-10-04 19:58:13 -0400

    Create Proyecto1.java

---
+++ Unidad 2 JAVA/proyecto1/Proyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+ package com.mycompany.proyecto1;
+ import java.lang.*;
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        String nombre="Alejandro" ;
+        System.out.println(nombre );
+    }
+}
